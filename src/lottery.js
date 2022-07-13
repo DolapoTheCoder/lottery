974de1c1342126c6dcfb6f1e10909d4fbb5ec87c
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2022-07-13 18:31:11 +0100

    master

---
+++ src/lottery.js
@@ -0,0 +1,9 @@
+import web3 from "./web3";
+
+//address of deployed contract.
+const address = '0x11Eab2CE32131358a8867dDd0b065261A52C7f49';
+
+const abi = [{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}];
+
+//local copy of lottery contract
+export default new web3.eth.Contract(abi, address);
